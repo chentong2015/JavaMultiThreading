4c46d0c5e26ecfde45d6db1dfc254b90a37ecab0
chent5 <chen2015tong@gmail.com>
2025-09-05 14:35:34 +0200

    update thread pool work testing

--- java-thread-pool/src/main/java/work_testing/TestThreadPoolTasks.java
+++ java-thread-pool/src/main/java/work_testing/TestThreadPoolTasks.java
@@ -1,27 +1,29 @@
 package work_testing;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 // TODO. 并发任务场景:
 // 从主线程新开一个异步线程，和主线程并发执行(执行的逻辑相互独立)
 // 确保两个线程执行结束，获取各自线程的返回结果
 public class TestThreadPoolTasks {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Future<Long> countFuture = executor.submit(() -> {
             System.out.println(Thread.currentThread().getName());
             try {
-                Thread.sleep(15000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             System.out.println("Sub Thread finished");
             return 10L;
         });
+
+        // TODO. 这里会阻塞主线程，导致main线程无法并行 ==> 同步的效果
+        long result = countFuture.get(6, TimeUnit.SECONDS);
+        System.out.println(result);
 
         System.out.println("Run main thread");
         try {
@@ -35,6 +37,7 @@ public class TestThreadPoolTasks {
         // 如果在timeout的时间内子线程的task还没有结束，则报错
         try {
             Long count = countFuture.get(6, TimeUnit.SECONDS);
+            count = countFuture.get(6, TimeUnit.SECONDS);
             System.out.println("count = " + count);
         } catch (Exception e) {
             e.printStackTrace();
